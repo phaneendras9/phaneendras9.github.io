a2e3172970610d81cae5e58024aa12034826a4f4
phaneendras9 <siddanaphaneendra9@gmail.com>
2024-10-26 12:11:41 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@
 document.querySelector('.hamburger-menu').addEventListener('click', () => {
     alert("Hamburger menu clicked!");
 });
+// JavaScript to toggle dropdown menu
+document.getElementById('hamburger').addEventListener('click', () => {
+    const dropdownMenu = document.getElementById('dropdown-menu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+});
 
 
