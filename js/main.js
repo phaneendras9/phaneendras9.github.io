3c72956ee0954a020d5f442b2dd28ce59e5ebfe5
phaneendras9 <siddanaphaneendra9@gmail.com>
2024-10-26 12:12:35 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-// JavaScript to add interactivity (Optional)
-document.querySelector('.hamburger-menu').addEventListener('click', () => {
-    alert("Hamburger menu clicked!");
-});
+// // JavaScript to add interactivity (Optional)
+// document.querySelector('.hamburger-menu').addEventListener('click', () => {
+//     alert("Hamburger menu clicked!");
+// });
 // JavaScript to toggle dropdown menu
 document.getElementById('hamburger').addEventListener('click', () => {
     const dropdownMenu = document.getElementById('dropdown-menu');
